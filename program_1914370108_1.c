55221e0b654a0c660f92fc823c453c4be08ea9ce
fadlirivansyah12 <55773490+fadlirivansyah12@users.noreply.github.com>
2019-11-01 15:40:11 +0700

    Add files via upload

---
+++ program_1914370108_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int NA= 74;
+    if (NA >= 70){
+        printf("lulus.");
+    } else {
+       if  (NA <= 70){
+        printf("tidak lulus.");
+       }return 0;
+    }
+}
